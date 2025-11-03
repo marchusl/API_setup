9c8ddb6312c16f74aa9f995443e36414fd11fed8
Marcus Lundbak <marchusl@gmail.com>
2025-11-03 14:48:29 +0100

    .

--- tasks/GetScreenshot.py
+++ tasks/GetScreenshot.py
@@ -4,5 +4,5 @@ def take_screenshot(page, job_id):
     filepath = f"static/screenshots/{job_id}.png"
     page.screenshot(path=filepath, full_page=True)
 
-    public_url = f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')}/static/screenshots/{job_id}.png"
-    return {"screenshot_url": "https://yourapp.com/static/screenshots/<job_id>.png"}
+    screenshot_url = f"https://api-setup-nd3w.onrender.com/static/screenshots/{job_id}.png"
+    return {"screenshot_url": screenshot_url}
